2a058ad9767ac9b6b3cf97ec77eba151423aa02a
LiloFr <88384549+LiloFr@users.noreply.github.com>
2022-01-25 22:50:04 +0300

    Update

--- conj.py
+++ conj.py
@@ -1,8 +1,39 @@
-x = int(input('Введите x: '))
+x = 0
+nums = []
+
+
+def input_f():
+    n = int(input('Введите натуральное число:'))
+    if n <= 0:
+        print('Неверные данные')
+        input_f()
+    global x
+    x = n
+
+
 def x2(x):
     x = x / 2
     return collatz(x)
 
-def x3_1(x):
-    x = x*3+1
-    return collatz(x)
+
+def x3_1(num):
+    num = num * 3 + 1
+    return collatz(num)
+
+
+def collatz(n):
+    global nums
+    if n == 1:
+        nums.append(int(n))
+        print(nums)
+        return n
+    elif n % 2 == 0:
+        nums.append(int(n))
+        return x2(n)
+    else:
+        nums.append(int(n))
+        return x3_1(n)
+
+
+input_f()
+collatz(x)
